dec4be350bbd9f899dd60e87b282236e161794f5
unknown <tobiasprieto10@gmail.com>
2025-04-24 22:18:59 -0300

    Se agrega UCS

--- tp-pathfinding/src/pathfinder/search/ucs.py
+++ tp-pathfinding/src/pathfinder/search/ucs.py
@@ -1,5 +1,5 @@
 from ..models.grid import Grid
-from ..models.frontier import StackFrontier
+from ..models.frontier import PriorityQueueFrontier
 from ..models.solution import NoSolution, Solution
 from ..models.node import Node
 
@@ -24,4 +24,51 @@ class UniformCostSearch:
         # Add the node to the explored dictionary
         explored[node.state] = True
         
+        # Return if the node contains a goal state
+        if node.state == grid.end:
+            return Solution(node, explored)
+        
+        # Initialize the frontier with the initial node
+        # In this example, the frontier is a queue
+        frontier = PriorityQueueFrontier()
+        frontier.add(node, node.cost)
+
+        # do
+        while True:
+
+            if frontier.is_empty():
+                return NoSolution(explored)
+            
+            node = frontier.pop()
+
+            successors = grid.get_neighbours(node.state)
+
+        #     forall a in problema.acciones(n.estado) do
+            for action in successors:
+                new_state = successors[action]
+                
+                # Check if the successor is not reached
+                if new_state not in explored:
+
+                    # Initialize the son node
+                    new_node = Node("", new_state,
+                                    node.cost + grid.get_cost(new_state),
+                                    parent=node, action=action)
+
+                    # Mark the successor as reached
+                    explored[new_state] = True
+
+                    # Return if the node contains a goal state
+                    # In this example, the goal test is run
+                    # before adding a new node to the frontier
+                    if new_state == grid.end:
+                        return Solution(new_node, explored)
+
+                    # Add the new node to the frontier
+                    frontier.add(new_node,new_node.cost)
+
+
+
+
+
         return NoSolution(explored)
